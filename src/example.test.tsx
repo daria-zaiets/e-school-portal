e31fc7e33db788fa52b8eaea3f5d86912607732e
Daria Zaiets <daria.regist@gmail.com>
2025-04-08 17:52:55 +0300

    feat: add Vitest
    - Created an example test file.
    - Added Jest DOM matchers by creating `setupTests.ts`.
    - Configured Vite to use Vitest for testing with a setup file.

---
+++ src/example.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders Hello component', () => {
+    render(<App />)
+    expect(screen.getByText('Click on the Vite and React logos to learn more')).toBeInTheDocument()
+})
